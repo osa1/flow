7f8dd214d695218c595cdf9c3469ea0b85e5526c
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-08-26 08:41:48 +0000

    One more lexer test

--- src/lexer.rs
+++ src/lexer.rs
@@ -726,6 +726,12 @@ mod test_lexer {
                       ]));
     }
 
+    #[test]
+    fn lexer_nums() {
+        let nums = "3.0 3.1416 314.16e-2 0.31416E1 34e1 0x0.1E 0xA23p-4  0X1.921FB54442D18P+1";
+        assert_eq!(tokenize(nums).unwrap().len(), 8);
+    }
+
     fn is_lua_file(s : &str) -> bool {
         let len = s.len();
         len > 4 && &s[ len - 4 .. ] == ".lua"
