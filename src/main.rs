525ab053176d260ed6388693b66b1baee5b9fb1b
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-09-03 09:57:01 +0000

    Print lexer, parser, printer timings

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ mod test_utils;
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::time::Instant;
 
 fn main() {
     if let Some(file) = env::args().nth(1) {
@@ -29,15 +30,32 @@ fn main() {
             contents
         };
 
+        let before_lexer = Instant::now();
         let mut tokens : Vec<lexer::Tok> = lexer::tokenize(&contents).unwrap();
         tokens.push(lexer::Tok::EOS);
+        let after_lexer = Instant::now();
+        let lexer_time = after_lexer.duration_since(before_lexer);
+
+        println!("tokens: {:?}", tokens);
+
+        let before_parser = Instant::now();
         let parser = parser::Parser::new(&tokens);
         let defs = parser.parse();
+        let after_parser = Instant::now();
+        let parser_time = after_parser.duration_since(before_parser);
+
+        let before_printer = Instant::now();
         for (var, cfg) in defs.iter() {
             println!("var: {:?}", var);
             let mut str = String::new().into_bytes();
             cfg.print(&mut str);
             println!("{}", unsafe { String::from_utf8_unchecked(str) });
         }
+        let after_printer = Instant::now();
+        let printer_time = after_printer.duration_since(before_printer);
+
+        println!("Lexing took {} ms.", lexer_time.subsec_nanos() as f64 / 1000000f64);
+        println!("Parsing took {} ms.", parser_time.subsec_nanos() as f64 / 1000000f64);
+        println!("Printing took {} ms.", printer_time.subsec_nanos() as f64 / 1000000f64);
     }
 }
